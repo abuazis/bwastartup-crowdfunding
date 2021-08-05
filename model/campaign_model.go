860a0461717473a0185c678f1a412c8a39f63077
Arya Yunanta <aryayun90@gmail.com>
2021-08-05 08:50:55 +0700

    [Update] API documentation

--- model/campaign_model.go
+++ model/campaign_model.go
@@ -1,1 +1,11 @@
 package model
+
+type GetCampaignResponse struct {
+	Id               uint32 `json:"id"`
+	UserId           uint32 `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       uint64 `json:"goal_amount"`
+	CurrentAmount    uint64 `json:"current_amount"`
+}
--- model/web_response.go
+++ model/web_response.go
@@ -1,5 +1,7 @@
 package model
 
+const BASE_URL = "http://localhost:8080/"
+
 type WebResponse struct {
 	Code   uint32      `json:"code"`
 	Status string      `json:"status"`
